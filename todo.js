c1608278bf72b808724e40dfb2914965c97c517e
MassaSalah <massa.salah18@gmail.com>
2018-11-25 11:45:44 +0200

    Done: Styling and code inhancment

--- todo.js
+++ todo.js
@@ -1,9 +1,10 @@
+// show function is to be called whenever a change is made to the lis(add revpve sort check)
 function show() {
     const todos = get_todos();
 
     let html = '<ul>';
     todos.forEach((todo,i)=>{
-      html += '<li>'+todo.task+'<button class="remove" id="'+i+'">x</button><input type="checkbox" class ="check" id="'+i+'" ></li>';
+      html += '<li>'+todo.task+'<button class=" btn btn-danger remove" id="'+i+'">x</button><input type="checkbox" class ="check" id="'+i+'" ></li>';
     })
     html += '</ul>';
 
@@ -18,6 +19,7 @@ function show() {
         buttons[i].addEventListener('click', remove);
     }); 
 }
+
 //add function to add a new task from the text input to the items list 
 function add() {
      this.parentElement.children.item(1)!==null ? this.parentElement.removeChild(this.parentElement.childNodes[3]): null
@@ -34,15 +36,16 @@ function add() {
             } 
          }
         else {
-            const d = new Date();      
+            const date = new Date();      
             const todos = get_todos();
-            todos.push({'task':task , 'date':d ,'done':false});    
+            todos.push({'task':task , 'date':date ,'done':false});    
             localStorage.setItem('todo', JSON.stringify(todos));    
             this.value="";
             show();
         }
     }
 }
+
 //get the todos stored in the session 
 function get_todos() {
     let todos = [];
@@ -50,6 +53,7 @@ function get_todos() {
     todos_str !== null ? todos = JSON.parse(todos_str): null    
     return todos;
 }
+
 //remove function to remove an item from the list on button click 
 function remove() {
     const id = this.getAttribute('id');
@@ -58,6 +62,7 @@ function remove() {
     localStorage.setItem('todo', JSON.stringify(todos));
     show();
 }
+
 //check function to update the status of the check button
 function check() {
     const todos = get_todos();
@@ -65,27 +70,24 @@ function check() {
     localStorage.setItem('todo', JSON.stringify(todos));
     show();
 }
+
 //sort function based of date done tasks and alphabetical order
 function sort(){
-    const sortSelected = this.value;
+    const sorting = this.value;
     const todos = get_todos();
-    let sorting ='';
-
-    sortSelected === 'Date' ? sorting = 'date' 
-    : sortSelected === 'Done' ? sorting = 'done'
-    : sortSelected === 'Alphabetical' ? sorting = 'task' : null
-    
     todos.sort((a, b)=>{
         if(a[sorting] < b[sorting]) { return -1; }
         if(a[sorting] > b[sorting]) { return 1; }
         return 0;
     })
+    
 
     localStorage.setItem('todo', JSON.stringify(todos));
     this.value = "";
     show();
 
 }
+
 //add event listners on usser input and sort elements
 document.getElementById("task").addEventListener("keydown", add);
 document.getElementById("sort").addEventListener("click", sort)
